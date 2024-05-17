f2afb5c33fb793d83c747e976889c83b15323283
whgek506 <whgek506@naver.com>
2024-05-17 09:51:31 +0900

    1

---
+++ src/main/front/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Spring Boot + React + NCP Test</h1>
+
+      <img src-='./image/haha.jpg' alt='haha'/>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/test/java/org/example/springbootncpgradle/SpringBootNcpGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springbootncpgradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootNcpGradleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
